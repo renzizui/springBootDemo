ea44d42b14bbd21b1a776422254ba53f4b835ec3
tianlei <1073168451@qq.com>
2022-04-19 15:20:18 +0800

    springBootdemo 代码

---
+++ jpa-demo/src/main/java/github/snailclimb/jpademo/po/Person.java
@@ -0,0 +1,33 @@
+package github.snailclimb.jpademo.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Integer age;
+
+    private Long companyId;
+
+    private Long schoolId;
+
+    public Person(String name,Integer age,Long companyId){
+        this.name=name;
+        this.age=age;
+        this.companyId=companyId;
+    }
+}
